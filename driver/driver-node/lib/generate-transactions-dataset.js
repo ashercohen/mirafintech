710d26c27aa0da37fb195aeafe01c07671f177a3
Tamir E <tamir@rise.ai>
2021-05-15 19:15:04 +0300

    order of actions for processing

--- driver/driver-node/lib/generate-transactions-dataset.js
+++ driver/driver-node/lib/generate-transactions-dataset.js
@@ -1,7 +1,14 @@
 module.exports = async function generate_transaction_dataset(user){
+    //init customer
     const { ID,LIMIT_BAL,SEX, EDUCATION,MARRIAGE,AGE} = user; 
-    const {PAY_0,PAY_2,PAY_3,PAY_4,PAY_5,PAY_6} = user; 
-    const {BILL_AMT1,BILL_AMT2,BILL_AMT3,BILL_AMT4,BILL_AMT5,BILL_AMT6} = user;
-    const {PAY_AMT1,PAY_AMT2,PAY_AMT3,PAY_AMT4,PAY_AMT5,PAY_AMT6}  = user;
+    
+    //calculate real world defualt rate
+    const { BILL_AMT1,BILL_AMT2,BILL_AMT3,BILL_AMT4,BILL_AMT5,BILL_AMT6 } = user;
+    const { PAY_AMT1,PAY_AMT2,PAY_AMT3,PAY_AMT4,PAY_AMT5,PAY_AMT6 }  = user;
+    
+
+    //split payment to transactions
+    const { PAY_0,PAY_2,PAY_3,PAY_4,PAY_5,PAY_6 } = user; 
+    
       
 };
